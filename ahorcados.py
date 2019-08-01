b5a55999491b39afc2acbab7da8f245c811a7684
raicerk <raicerk@gmail.com>
2019-08-01 00:20:34 -0400

    Se finaliza logica de juego ahoracado

--- ahorcados.py
+++ ahorcados.py
@@ -11,7 +11,7 @@ IMAGES = ['''
         |
         |
         |
-        =========''','''
+        =========''', '''
 
     +---+
     |   |
@@ -81,9 +81,11 @@ WORDS = [
     'teclado'
 ]
 
+
 def random_word():
-    idx = random.randint(0,len(WORDS)-1)
+    idx = random.randint(0, len(WORDS)-1)
     return WORDS[idx]
+
 
 def display_board(hidden_word, tries):
     print(IMAGES[tries])
@@ -91,24 +93,43 @@ def display_board(hidden_word, tries):
     print(hidden_word)
     print('--- * --- * --- * --- * --- * --- * --- * ---')
 
+
 def run():
     word = random_word()
     hidden_word = ['-'] * len(word)
     tries = 0
 
     while True:
-        display_board(hidden_word,tries)
+        display_board(hidden_word, tries)
         current_letter = str(input('Escoge una letra: '))
 
         letter_indexes = []
         for idx in range(len(word)):
-            if word[idx] ==  current_letter:
+            if word[idx] == current_letter:
                 letter_indexes.append(idx)
 
-                
+        if len(letter_indexes) == 0:
+            tries += 1
+
+            if tries == 7:
+                display_board(hidden_word,tries)
+                print('')
+                print('Has perdido el juego!, La palabra correcta era "{}"'.format(word))
+                break
+        else:
+            for idx in letter_indexes:
+                hidden_word[idx] = current_letter
+
+            letter_indexes = []
+
+        try:
+            hidden_word.index('-')
+        except ValueError:
+            print('')
+            print('Felicidades, ganaste! La palabra es "{}"'.format(word))
+            break
 
 
 if __name__ == "__main__":
     print('B I E N V E N I D O S  A  A H O R C A D O S')
-    run()    
-    
+    run()
