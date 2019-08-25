f8ce0c82dcdbe31354b197f88c391e8792b21a29
raicerk <raicerk@gmail.com>
2019-08-25 03:48:38 -0400

    Se crea logica para recibir datos desde un formulario web

--- servidor/main.py
+++ servidor/main.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hola():
-    return render_template('index.html')
+@app.route(r'/', methods=['GET'])
+def contact_book():
+    return render_template('contact_book.html')
+
+
+@app.route(r'/add', methods=['GET','POST'])
+def add_contact():
+
+    if request.form:
+        print("hola")
+        print(request.form.get('name'))
+        print(request.form.get('phone'))
+        print(request.form.get('email'))
+    else:
+        print("adios")
+        print('hola')
+
+    return render_template('add_contact.html')
 #@app.route('/')
 #def hello_world():
 #    return 'Hola mundo.'
