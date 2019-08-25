e6f442b12ae42cb939d7570811341bc5d0f228a4
raicerk <raicerk@gmail.com>
2019-08-24 21:55:01 -0400

    Se crea sistema de template con jinja2

--- servidor/main.py
+++ servidor/main.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
-def hello_world():
-    return 'Hola mundo.'
+def hola():
+    return render_template('index.html')
+#@app.route('/')
+#def hello_world():
+#    return 'Hola mundo.'
 
 if __name__ == "__main__":
     app.run()
